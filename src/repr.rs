8d65439a4c15ddf5e6a84c41f6c443f9699897e7
rufevean <deerajmydearalways@gmail.com>
2024-09-07 15:27:57 +0530

    feat - added parser repr

--- src/repr.rs
+++ src/repr.rs
@@ -1,5 +1,7 @@
+
 use crate::lexer::lexer;
-use crate::models::TokenType;
+use crate::models::{TokenType, Token};
+use crate::parser::Parser;
 use crate::errors;
 use std::io::{self, Write};
 
@@ -24,14 +26,18 @@ pub fn interactive_lexer() {
 
         let tokens = lexer(trimmed_input);
 
-        for token in tokens {
-        if token.token_type == TokenType::Unknown{
-            errors::invalid_char(token.clone())
+        for token in &tokens {
+            if token.token_type == TokenType::Unknown {
+                errors::invalid_char(token.clone());
+            }
+            println!("{:?}", token);
         }
- 
-            println!("{:?}", token);     }
-}
 
+        let mut parser = Parser::new(tokens);
 
-
+        match parser.parse_statement() {
+            Ok(_) => println!("Parsing succeeded."),
+            Err(e) => println!("Parsing failed: {}", e),
+        }
+    }
 }
