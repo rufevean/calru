63bc06e5d003df915d239cd25ce3657a0ff50822
rufevean <deerajmydearalways@gmail.com>
2024-09-08 13:00:04 +0530

    refactor - integrate ast with parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,6 @@
 
 use crate::models::{TokenType, Token, Position};
+use crate::ast::{AST, ASTNode};
 
 pub struct Parser {
     tokens: Vec<Token>,
@@ -16,11 +17,11 @@ impl Parser {
             current_token: None,
             position : Position {line: 1, column : 1}
         };
-        parser.advance(); // Initialize the first token
+        parser.advance(); 
         parser
     }
 
-    pub fn parse_statement(&mut self) -> Result<(), String> {
+    pub fn parse_statement(&mut self) -> Result<AST, String> {
         if self.current_token_is(TokenType::Let){
             self.parse_let_decl()
         }else{
@@ -33,86 +34,124 @@ impl Parser {
         }
     }
 
-    pub fn parse_let_decl(&mut self) -> Result<(),String>{
-        if !self.current_token_is(TokenType::Let){
-            return Err(format!("Expected 'let' at position {:?}", self.position));
-        }
 
-        self.advance();
-
-        if !self.current_token_is(TokenType::Identifier){
-            return Err(format!("Expected identifier at position {:?}",self.position));
-        }
-
-        self.advance();
-
-                // Expect Type (int or float)
-        if !self.current_token_is(TokenType::IntType) && !self.current_token_is(TokenType::FloatType) {
-            return Err(format!("Expected type (int/float) at position {:?}", self.position));
-        }
-        self.advance();
-
-
-        self.parse_assign_expr()?;
-
-
-        Ok(())
-
-        
+pub fn parse_let_decl(&mut self) -> Result<AST, String> {
+    if !self.current_token_is(TokenType::Let) {
+        return Err(format!("Expected 'let' at position {:?}", self.position));
     }
 
-    pub fn parse_expression(&mut self) -> Result<(),String>{
-
-        self.parse_term()?;
-
-        while self.current_token_is(TokenType::Operator) &&
-            (self.current_token.as_ref().unwrap().value == "+" ||
-            self.current_token.as_ref().unwrap().value=="-"){
-        self.advance();
-        self.parse_term()?;
-    }
-        Ok(())
-    }
-
-
-    pub fn parse_term(&mut self) -> Result<(),String>{
-
-        self.parse_factor()?;
-
-        while self.current_token_is(TokenType::Operator) &&
-        ( self.current_token.as_ref().unwrap().value == "*" ||
-        self.current_token.as_ref().unwrap().value == "/"){
     self.advance();
-    self.parse_factor();
+
+    if !self.current_token_is(TokenType::Identifier) {
+        return Err(format!("Expected identifier at position {:?}", self.position));
+    }
+
+    let variable = self.current_token.as_ref().unwrap().value.clone();
+    self.advance();
+
+    if !self.current_token_is(TokenType::IntType) && !self.current_token_is(TokenType::FloatType) {
+        return Err(format!("Expected type (int/float) at position {:?}", self.position));
+    }
+    self.advance();
+
+    let expression = self.parse_assign_expr()?;
+
+    Ok(AST::new(ASTNode::Assignment {
+        variable,
+        expression: Box::new(expression),
+    }))
 }
-Ok(())
+
+pub fn parse_expression(&mut self) -> Result<AST, String> {
+    let mut left = self.parse_term()?; 
+
+    while self.current_token_is(TokenType::Operator) &&
+          (self.current_token.as_ref().unwrap().value == "+" ||
+           self.current_token.as_ref().unwrap().value == "-") {
+
+        let operator = self.current_token.as_ref().unwrap().value.clone();
+        self.advance();
+        let right = self.parse_term()?;
+
+        left = AST::new(ASTNode::BinaryOperation {
+            operator,
+            left: Box::new(left),
+            right: Box::new(right),
+        });
     }
 
-    pub fn parse_factor(&mut self) -> Result<(),String>{
-        match self.current_token{
-            Some(ref token) if token.token_type == TokenType::Number || token.token_type == TokenType::Identifier => {
-                self.advance();
-                Ok(())
-            },
-            _ => Err(format!("Unexpected token {:?} at position {:?}",self.current_token,self.position))
-                    }
+    Ok(left) 
+}
+
+
+
+
+ 
+pub fn parse_term(&mut self) -> Result<AST, String> {
+    let mut left = self.parse_factor()?;
+
+    while self.current_token_is(TokenType::Operator) &&
+          (self.current_token.as_ref().unwrap().value == "*" ||
+           self.current_token.as_ref().unwrap().value == "/") {
+
+        let operator = self.current_token.as_ref().unwrap().value.clone();
+        self.advance();
+        let right = self.parse_factor()?; 
+
+        left = AST::new(ASTNode::BinaryOperation {
+            operator,
+            left: Box::new(left),
+            right: Box::new(right),
+        });
     }
 
-    pub fn parse_assign_expr(&mut self)-> Result<(),String>{
-        if self.current_token_is(TokenType::Assign){
+    Ok(left) 
+}
+
+
+
+pub fn parse_factor(&mut self) -> Result<AST, String> {
+    match self.current_token {
+        Some(ref token) if token.token_type == TokenType::Number => {
+            let value = token.value.parse::<f64>()
+                .map_err(|_| format!("Invalid number format at position {:?}", self.position))?;
             self.advance();
-            self.parse_expression()?;
-            if self.current_token_is(TokenType::Termination){
-                self.advance();
-                Ok(())
-            }else{
-                Err(format!("Expected ';' at position {:?}",self.position))
-            }
-            
-        }else{
-            Err(format!("Expected  ':=' at position {:?}",self.position))
-        }
+            Ok(AST::new(ASTNode::Number(value)))
+        },
+        Some(ref token) if token.token_type == TokenType::FloatNumber => {
+            let value = token.value.parse::<f64>()
+                .map_err(|_| format!("Invalid float format at position {:?}", self.position))?;
+            self.advance();
+            Ok(AST::new(ASTNode::Number(value)))
+        },
+        Some(ref token) if token.token_type == TokenType::Identifier => {
+            let value = token.value.clone();
+            self.advance();
+            Ok(AST::new(ASTNode::Identifier(value)))
+        },
+        _ => Err(format!("Unexpected token {:?} at position {:?}", self.current_token, self.position)),
     }
+}
+
+
+
+
+pub fn parse_assign_expr(&mut self) -> Result<AST, String> {
+    if !self.current_token_is(TokenType::Assign) {
+        return Err(format!("Expected ':=' at position {:?}", self.position));
+    }
+
+    self.advance();
+    let expr = self.parse_expression()?;
+
+    if !self.current_token_is(TokenType::Termination) {
+        return Err(format!("Expected ';' at position {:?}", self.position));
+    }
+
+    self.advance();
+    Ok(expr)
+}
+
     
             /* UTIL METHODS */
 
@@ -124,9 +163,9 @@ Ok(())
         if self.current_index < self.tokens.len() {
             self.current_token = Some(self.tokens[self.current_index].clone());
             self.update_position() ;
-            self.current_index += 1; // Move to the next token
+            self.current_index += 1; 
         } else {
-            self.current_token = None; // End of token list
+            self.current_token = None; 
         }
     }
 
